57062e6a76dec625fe800761eb83c9a70960c795
minghan9456 <oo7680485@hotmail.com>
2020-01-10 15:51:57 +0000

    john udpate

--- public/js/chat.js
+++ public/js/chat.js
@@ -52,10 +52,18 @@ var CHAT = (function() {
           return false;
         });
         socket.on('chat message', function(msg){
-          $('#messages').append($('<li>').text(profile.name + " : " + msg));
+          let text = "[" + getDateTime() + "] " + profile.name + " : " + msg;
+          $('#messages').append($('<li>').text(text));
           window.scrollTo(0, document.body.scrollHeight);
         });
       });
+
+      function getDateTime() {
+        let today = new Date();
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        return date+' '+time;
+      }
   }
 
   return {
