18f60c6348f34d9fd70643924c6711a14e3e5e2c
minghan9456 <oo7680485@hotmail.com>
2020-01-12 06:05:52 +0000

    john new example constant

---
+++ config/constant.example.js
@@ -0,0 +1,10 @@
+exports.ENVIRONMENT = 'development';
+
+exports.PORT = 9527;
+
+exports.MYSQL_HOST = '';
+exports.MYSQL_DATABASE = '';
+exports.MYSQL_USER = '';
+exports.MYSQL_PASSWORD = '';
+
+exports.google_client_id = '';
